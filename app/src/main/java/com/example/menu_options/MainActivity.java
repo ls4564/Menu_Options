15f41338eb2a358ee543cf3d3623ee5fc2a4c3ae
Lior1254 <liorshemtov2008@gmail.com>
2024-12-04 16:25:43 +0200

    close to finish

--- app/src/main/java/com/example/menu_options/MainActivity.java
+++ app/src/main/java/com/example/menu_options/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText eD1,eD2;
     TextView tV1;
 
-    Double num1,num2;
+    //Double num1,num2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,13 +42,21 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean check_input(String input)
     {
-        if(input.equals(".") || input.equals("-") || input.equals(".-") || input.equals("-.") || input.equals("+") || input.equals(".+") || input.equals("+."))
+        if(input.isEmpty() || input.equals(".") || input.equals("-") || input.equals(".-") || input.equals("-.") || input.equals("+") || input.equals(".+") || input.equals("+."))
         {
             return false;
         }
         return true;
     }
 
+    public boolean check_zero(double nun)
+    {
+        if(nun == 0)
+        {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu)
@@ -58,29 +67,53 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean onOptionsItemSelected (MenuItem item)
     {
-        if(check_input())//check input
+        String str_eD1,str_eD2;
+        str_eD1 = eD1.getText().toString();
+        str_eD2 = eD2.getText().toString();
+
+        if(check_input(str_eD1) && check_input(str_eD2))//check input
         {
+            Double num_eD1 = Double.parseDouble(str_eD1);
+            Double num_eD2 = Double.parseDouble(str_eD2);
+            Double ans;
             int id = item.getItemId();
             if(id == R.id.menu_plus)
             {
-
+                ans = num_eD1 + num_eD2;
+                tV1.setText(""+ ans);
             }
             else if(id == R.id.menu_minus)
             {
-
+                ans = num_eD1 - num_eD2;
+                tV1.setText(""+ ans);
             }
             else if(id == R.id.menu_mul)
             {
-
+                ans = num_eD1 * num_eD2;
+                tV1.setText(""+ ans);
             }
             else if(id == R.id.menu_div)
             {
-
+                if(check_zero(num_eD2))
+                {
+                    ans = num_eD1 / num_eD2;
+                    tV1.setText(""+ ans);
+                }
+                else
+                {
+                    Toast.makeText(this,"Error: Can't div in zero",Toast.LENGTH_LONG).show();
+                }
             }
             else
             {
-
+                eD1.setText("");
+                eD2.setText("");
+                tV1.setText("");
             }
+        }
+        else
+        {
+            Toast.makeText(this,"Error: illegal input",Toast.LENGTH_LONG).show();
         }
 
 
